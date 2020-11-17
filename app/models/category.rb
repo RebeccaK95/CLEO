79fd7dd0b57aef0c52948f2a209ace6ab968b7db
Florence Bastin <bastin.florence@gmail.com>
2020-11-17 11:27:26 +0100

    categories created

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :tips, :challenges
+  CATEGORIES = ["Bathroom", "Daily", "Energy", "Food", "Shopping", "Transports"]
+  validation :name, inclusion:{in:CATEGORIES}
 end
