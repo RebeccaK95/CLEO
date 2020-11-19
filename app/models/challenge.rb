f317b95fc2e3b8aafbf6ddb8ecd45ef9267da068
Rebecca Kornwolf <rebeccakornwolf@gmail.com>
2020-11-19 16:51:59 +0100

    merge error

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,10 +2,6 @@ class Challenge < ApplicationRecord
   belongs_to :category
   has_many :users, through: :user_challenges
 
-<<<<<<< HEAD
-=======
-
->>>>>>> a30cec8773ce58c2f29c1013578d4a3a466c1fca
   include PgSearch::Model
   pg_search_scope :pg_search,
     against: [:name, :description],
