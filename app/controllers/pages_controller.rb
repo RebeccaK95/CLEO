bf7f798d06011176384084931cb890d6c842f3f6
camille colet <camcolet@gmail.com>
2020-11-23 17:10:35 +0100

    ajax tried

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
     @challenges = Challenge.first(3)
     @tips = Tip.all
     @show_page = true
-    @user_challenges = UserChallenge.where(user_id: current_user.id)
+    @user_challenges = UserChallenge.where(user_id: current_user&.id)
     @ongoing_challenges = @user_challenges.where(status: "in_progress")
   end
 
