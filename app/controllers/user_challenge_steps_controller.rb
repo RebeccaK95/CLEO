4d928a4c68ffeae04c409e0f415d3a6a7dbb77be
camille colet <camcolet@gmail.com>
2020-11-25 12:11:42 +0100

    alert ok

--- app/controllers/user_challenge_steps_controller.rb
+++ app/controllers/user_challenge_steps_controller.rb
@@ -1,7 +1,7 @@
-class UserChallengeStepsController < ApplicationController  
+class UserChallengeStepsController < ApplicationController
   def update
     user_challenge_step = UserChallengeStep.find(params[:id])
     user_challenge_step.update(status: "completed")
-    redirect_to challenge_path(user_challenge_step.user_challenge.challenge) 
-  end 
-end 
+    redirect_to challenge_path(user_challenge_step.user_challenge.challenge), notice: "Good job, the planet is greener already !"
+  end
+end
